3a222dbd6113b18827ede45b6b712de1412cff1d
HolyDoge97 <98220384+HolyDoge97@users.noreply.github.com>
2022-08-30 18:17:03 +0900

    .
    .

--- src/main/java/com/fastcampus/nodemo/Controller/D2_getController.java
+++ src/main/java/com/fastcampus/nodemo/Controller/D2_getController.java
@@ -1,15 +1,27 @@
 package com.fastcampus.nodemo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fastcampus.nodemo.Services.Services;
+
+// IM CLIENT!!
 
 @RestController
 @RequestMapping("/api/hi")
 public class D2_getController {
 
-    @GetMapping("/name")
-    public String nameReturn() {
-        return "Seok hwan";
+    @Autowired
+    Services service;
+
+    @GetMapping("")
+    public String getHello() {
+        return service.hello();
+    }
+
+    @GetMapping("/hihi")
+    public String getHihi() {
+        return service.hihi();
     }
 }
--- src/main/java/com/fastcampus/nodemo/Services/Services.java
+++ src/main/java/com/fastcampus/nodemo/Services/Services.java
@@ -2,6 +2,7 @@ package com.fastcampus.nodemo.Services;
 
 import java.net.URI;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -11,15 +12,36 @@ public class Services {
 
     public String hello() {
         URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost")
-                .path("/api/hello")
+                .fromUriString("http://localhost:9090")
+                .path("/api/server")
                 .encode()
                 .build()
                 .toUri();
-        System.out.println(uri.toString());
+
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        return result;
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        System.out.println(uri.toString() + "\n" +
+                result.getBody() + "\n" +
+                result.getStatusCode());
+
+        return "Done";
+
+    }
+
+    public String hihi() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hihi")
+                .encode()
+                .build()
+                .toUri();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        System.out.println(uri.toString() + "\n" +
+                result.getBody() + "\n" +
+                result.getStatusCode());
+
+        return "Done";
 
     }
 
