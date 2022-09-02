fdd3d7ce85e864ae872160dd2b7d8a73a6169586
HolyDoge97 <98220384+HolyDoge97@users.noreply.github.com>
2022-09-02 13:54:34 +0900

    z

--- src/main/java/com/fastcampus/nodemo/Controller/basicController.java
+++ src/main/java/com/fastcampus/nodemo/Controller/basicController.java
@@ -10,6 +10,7 @@ import com.fastcampus.nodemo.JPA.freeboard;
 import com.fastcampus.nodemo.repository.boardRepo;
 import lombok.RequiredArgsConstructor;
 
+// feature / Test에 푸시할 내용임
 @Controller // JSON 형태 결과값을 반환해줌 (@ResponseBody가 필요없음)
 @RequiredArgsConstructor // final 객체를 Constructor Injection 해줌. (Autowired 역할)
 public class basicController {
@@ -30,7 +31,7 @@ public class basicController {
     public String showList(Model model) {
         List<freeboard> showList = repo.findAll();
         model.addAttribute(showList);
-        System.out.println("Done\n============");
+        System.out.println("Done\n===========s=");
         return "list";
     }
 
